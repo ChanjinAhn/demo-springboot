1969b72db7a94ffca38df9524dc23188ae5bdd2c
chanjin_ahn <infilljean@gmail.com>
2022-01-17 01:29:25 +0900

    add: init_project

---
+++ src/main/java/example/demospringboot/controller/MainController.java
@@ -0,0 +1,15 @@
+package example.demospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @GetMapping("/main")
+    public String main(@RequestParam(name = "name", required = false, defaultValue = "Wolrd") String name, Model model){
+        model.addAttribute("name", name);
+        return "main";
+    }
+}
